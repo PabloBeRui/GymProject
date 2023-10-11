d1e7ddb1269f7eeb8a2b58e6d899dabd312a4fd8
PabloBeRui <121795105+PabloBeRui@users.noreply.github.com>
2023-10-11 10:10:48 +0200

    first commit

---
+++ backend/backend-model/src/main/java/com/campusdual/backend/model/core/dao/UserDao.java
@@ -0,0 +1,28 @@
+package com.campusdual.backend.model.core.dao;
+
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+
+
+@Lazy
+@Repository(value = "UserDao")
+@ConfigurationFile(
+	configurationFile = "dao/UserDao.xml",
+	configurationFilePlaceholder = "dao/placeholders.properties")
+public class UserDao extends OntimizeJdbcDaoSupport {
+
+    public static final String ID = "user_";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String NAME = "name";
+    public static final String SURNAME = "surname";
+    public static final String NIF = "nif";
+    public static final String USERBLOCKED = "userblocked";
+    public static final String LASTPASSWORDUPDATE = "lastpasswordupdate";
+    public static final String FIRSTLOGIN = "firstlogin";
+
+}
